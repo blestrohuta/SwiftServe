f339dac7b948e40a3e9594e0f8eab3d1e206f1a3
Blestro Bellarmino <blestrohuta@gmail.com>
2023-05-11 11:47:37 +0700

    edit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router()
-const first = require('./firstRoute');
-const second = require('./secondRoute');
+const home = require('./home')
+const login = require('./login')
 
-router.use('/', first)
-router.use('/', second)
+router.use('/', home)
+router.use('/user', login)
 
 module.exports = router

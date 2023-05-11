2e5b565f760b02a097280ca19f3e6e6be88968f3
Alfandani araniri aditya putra <alfandnap@gmail.com>
2023-05-11 23:09:07 +0700

    fail nodemailer

--- controllers/alfan.js
+++ controllers/alfan.js
@@ -1,5 +1,6 @@
 const formatNumber = require('../helpers/helper')
 const { Category, Item, OrderDemand, Profile, User } = require('../models')
+// var nodemailer = require('nodemailer');
 
 class Controller1 {
     static orderByUserId(req, res) {
@@ -19,10 +20,27 @@ class Controller1 {
             msg = req.query.errors.split(',');
         }
         const { userId, itemId } = req.params
+
         res.render('formAddOrder', { userId, itemId, msg })
     }
 
     static postAddOrderByUserId(req, res) {
+
+        // let transporter = nodemailer.createTransport({
+        //     service: 'gmail',
+        //     auth: {
+        //         type: 'OAuth2',
+        //         user: 'ranirilearning@gmail.com',
+        //         pass: 'ranirilearning123',
+        //         clientId: '1096894214240-su0qk9uv8ojtnnpfvr11m7ea4na8pk49.apps.googleusercontent.com',
+        //         clientSecret: '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',
+        //         refreshToken: '1//04cJChGIdiap8CgYIARAAGAQSNwF-L9IrGOsQIEfeaovhxo1EcqF2lzWdqMseycOtbZTd5t7f63i6qBOQveXq1pyiMwhd2uzZzz8',
+        //         tls: {
+        //             rejectUnauthorized: false
+        //         }
+        //     }
+        // });
+
         const { userId, itemId } = req.params
         const { startDate, duration } = req.body
         let price;
@@ -31,11 +49,29 @@ class Controller1 {
                 if (price) {
                     price = item.price * duration
                 }
-                const newOrder = { UserId : userId, ItemId : itemId, startDate, duration, price }
+                const newOrder = { UserId: userId, ItemId: itemId, startDate, duration, price }
                 return OrderDemand.create(newOrder)
             })
             .then(order => {
+                return User.findByPk(userId)
+            })
+            .then(user => {
                 res.redirect(`/order/${userId}`)
+
+                // let mailOptions = {
+                //     from: 'ranirilearning@gmail.com',
+                //     to: `ranirilearning@gmail.com`,
+                //     subject: 'pair project phase 1',
+                //     text: 'Hi from pair project phase 1'
+                // };
+
+                // transporter.sendMail(mailOptions, function (err, data) {
+                //     if (err) {
+                //         console.log("Error " + err);
+                //     } else {
+                //         console.log("Email sent successfully");
+                //     }
+                // });
             })
             .catch(err => {
                 if (err.name == 'SequelizeValidationError') {
@@ -44,16 +80,19 @@ class Controller1 {
                     });
                     res.redirect(`/order/add/${userId}/${itemId}?errors=${errors}`)
                 } else {
+                    console.log(err);
                     res.send(err)
                 }
             })
     }
 
     static deleteOrder(req, res) {
-        const {orderDemandId, userId} = req.params
-        OrderDemand.destroy({where: {
-            id: orderDemandId
-        }})
+        const { orderDemandId, userId } = req.params
+        OrderDemand.destroy({
+            where: {
+                id: orderDemandId
+            }
+        })
             .then(order => {
                 res.redirect(`/order/${userId}`)
             })
@@ -63,12 +102,12 @@ class Controller1 {
     }
 
     static orderDetail(req, res) {
-        const {orderDemandId, userId} = req.params
+        const { orderDemandId, userId } = req.params
         OrderDemand.findByPk(orderDemandId, {
             include: ['Item']
         })
             .then(order => {
-                res.render('orderDetail', {order, formatNumber})
+                res.render('orderDetail', { order, formatNumber })
             })
             .catch(err => {
                 res.send(err)
@@ -76,12 +115,12 @@ class Controller1 {
     }
 
     static updateOrder(req, res) {
-        const {orderDemandId, userId} = req.params
-        OrderDemand.findByPk(orderDemandId, {include: ['Item']})
+        const { orderDemandId, userId } = req.params
+        OrderDemand.findByPk(orderDemandId, { include: ['Item'] })
             .then(order => {
                 const duration = order.duration + 1;
                 const price = order.Item?.price * duration;
-                return OrderDemand.update({duration, price}, {
+                return OrderDemand.update({ duration, price }, {
                     where: {
                         id: orderDemandId
                     }
